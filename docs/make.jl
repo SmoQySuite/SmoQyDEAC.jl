61df20065ad9b54077c4f6590c50c182fe08bf12
sandimas <james.neuhaus@gmail.com>
2023-12-19 10:37:54 -0500

    dev init

--- docs/make.jl
+++ docs/make.jl
@@ -27,7 +27,7 @@ makedocs(;
     sitename="SmoQyDEAC.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://SmoQySuite.github.io/SmoQyDEAC.jl/stable",
+        canonical="https://SmoQySuite.github.io/SmoQyDEAC.jl/dev",
         edit_link="main",
         assets=String[],
     ),
