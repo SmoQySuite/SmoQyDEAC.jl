67d82251a5c66e58e052583cf779d02bbc9087cf
sandimas <james.neuhaus@gmail.com>
2024-02-07 08:56:53 -0500

    fixed runtests.jl for new checkpoint file API

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,7 @@ using Random
     Greens = [0.5,0.4,0.3,0.2,0.1,0.05,0.1,0.2,0.3,0.4,0.5]
     Greens_std = zeros(Float64,11) .+ 0.02
 
-    @test typeof( SmoQyDEAC.DEAC_Std(Greens,Greens_std,1.0,collect(LinRange(0.0,1.0,11)),collect(LinRange(-10.0,10.0,401)),"time_fermionic",2,10,"x.jld2",".")) == Dict{String,Any}
+    @test typeof( SmoQyDEAC.DEAC_Std(Greens,Greens_std,1.0,collect(LinRange(0.0,1.0,11)),collect(LinRange(-10.0,10.0,401)),"time_fermionic",2,10,"x.jld2","DEAC_checkpoint.jld2")) == Dict{String,Any}
     rm("x.jld2")
 
 
